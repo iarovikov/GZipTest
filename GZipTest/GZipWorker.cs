472087ecc0e4f7ff8225c1e26bdc6573ee517177
vadimyarovikov <vadim.yarovikov@gmail.com>
2018-02-07 11:25:06 +0100

    Implement compression of the file.

--- GZipTest/GZipWorker.cs
+++ GZipTest/GZipWorker.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.IO.Compression;
 
 namespace GZipTest
 {
@@ -6,7 +8,29 @@ namespace GZipTest
     {
         public void Compress(FileInfo fileToCompress)
         {
-            throw new System.NotImplementedException();
+            using (FileStream inputStream = fileToCompress.OpenRead())
+            {
+                if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) == FileAttributes.Hidden
+                    || fileToCompress.Extension == ".gz")
+                {
+                    throw new InvalidOperationException("File is hidden or already compressed");
+                }
+                using (FileStream outFile = File.Create(fileToCompress.FullName + ".gz"))
+                {
+                    using (GZipStream gZipStream = new GZipStream(outFile,
+                        CompressionMode.Compress))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int numRead;
+                        while ((numRead = inputStream.Read(buffer, 0, buffer.Length)) != 0)
+                        {
+                            gZipStream.Write(buffer, 0, numRead);
+                        }
+                        Console.WriteLine("Compressed {0} from {1} to {2} bytes.",
+                            fileToCompress.Name, fileToCompress.Length, outFile.Length);
+                    }
+                }
+            }
         }
 
         public void Decompress(FileInfo fileToDecompress)
