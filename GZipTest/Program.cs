e9f2b8bb975df0cdd35459738a00a8defdfb04e7
vadimyarovikov <vadim.yarovikov@gmail.com>
2018-02-07 10:44:23 +0100

    Parse input args.

--- GZipTest/Program.cs
+++ GZipTest/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace GZipTest
+﻿using System.IO;
+
+namespace GZipTest
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            //TODO: input parameters validation
+            var command = args[0];
+            var inputFile = new FileInfo(args[1]);
+            var ouputFile = new FileInfo(args[2]);
         }
     }
 }
