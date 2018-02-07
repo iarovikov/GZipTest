ce7af33d86714c4b31226c3d1ca63ab47a97b746
vadimyarovikov <vadim.yarovikov@gmail.com>
2018-02-07 10:49:37 +0100

    Add GZipWorker - implementation of ICompressor and IDecompressor

---
+++ GZipTest/GZipWorker.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace GZipTest
+{
+    public class GZipWorker : ICompressor, IDecompressor
+    {
+        public void Compress(FileInfo fileToCompress)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Decompress(FileInfo fileToDecompress)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
